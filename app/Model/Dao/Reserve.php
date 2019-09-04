e0fee96456ce0611e28cb5961339faf24f9a2bcb
wkmkymt <wkmkymt@gmail.com>
2019-09-05 03:03:35 +0900

    Init model

---
+++ app/Model/Dao/Reserve.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Model\Dao;
+
+/**
+ * Class Reserve
+ *
+ * Reserveテーブルを扱う Classです
+ * DAO.phpに用意したCRUD関数以外を実装するときに、記載をします。
+ *
+ * @author wkmkymt <wkmkymt@gmail.com>
+ * @since 2019/09/05
+ * @package Model\Dao
+ */
+class Reserve extends Dao
+{ }
