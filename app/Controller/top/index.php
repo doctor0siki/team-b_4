a554cea564a8216003a0d23c24ab2dbf524f9b94
wkmkymt <wkmkymt@gmail.com>
2019-09-05 17:43:12 +0900

    Fix dd

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Model\Dao\User;
 use Model\Dao\Topic;
 use Slim\Http\Request;
 use Slim\Http\Response;
@@ -8,20 +7,15 @@ use Slim\Http\Response;
 // TOPページのコントローラ
 $app->get('/', function (Request $request, Response $response) {
 
-    // User DAO, topic DAOをインスタンス化
-    $user = new User($this->db);
+    // DAO, topic DAOをインスタンス化
     $topic = new Topic($this->db);
 
     // セッションに登録されたユーザIDを取得
     $user_id = $this->session["user_info"]["id"];
 
     $data = [];
-    if ($user_id) {
-        $data["user"] = $user->getUser($user_id);
+    if ($user_id)
         $data["topics"] = $topic->getTopicList($user_id);
-    }
-
-    dd($data);
 
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
