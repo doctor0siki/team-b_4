aaeb34d941c1c83a2488272e27da56bbdeef7558
wkmkymt <wkmkymt@gmail.com>
2019-09-06 01:13:43 +0900

    Add getter of reserve

--- app/Controller/villa/index.php
+++ app/Controller/villa/index.php
@@ -1,19 +1,27 @@
 <?php
 
 use Model\Dao\Villa;
+use Model\Dao\Reserve;
+use Model\Calendar;
+
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Villaページのコントローラ
 $app->get('/villa/{ villa_id }[/]', function (Request $request, Response $response, $args) {
 
-    // Villa DAOをインスタンス化
+    // Villa DAO & Reserve DAOをインスタンス化
     $villa = new Villa($this->db);
+    $reserve = new Reserve($this->db);
 
-    // Villaを取得し、戻り値をresultに格納します
+    $cal = Calendar::getCalArray(date("Ym"));
+
+    // 各データを格納
     $data = [
         "villa" => $villa->getVilla($args["villa_id"]),
-        "spots" => $villa->getSpotByVilla(($args["villa_id"]))
+        "spots" => $villa->getSpotByVilla(($args["villa_id"])),
+        "calendar" => $cal,
+        "reserve" => $reserve->getReserveByVilla($args["villa_id"], date("Y-m-d")),
     ];
 
     // Render index view
