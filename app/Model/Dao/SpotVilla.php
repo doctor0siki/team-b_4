dcdde329577d1ce050e5a42296ec0d2c728fd21a
wkmkymt <wkmkymt@gmail.com>
2019-09-05 13:20:49 +0900

    Fix class name

--- app/Model/Dao/SpotVilla.php
+++ app/Model/Dao/SpotVilla.php
@@ -3,14 +3,14 @@
 namespace Model\Dao;
 
 /**
- * Class SpotVIlla
+ * Class SpotVilla
  *
- * SpotVIllaテーブルを扱う Classです
+ * SpotVillaテーブルを扱う Classです
  * DAO.phpに用意したCRUD関数以外を実装するときに、記載をします。
  *
  * @author wkmkymt <wkmkymt@gmail.com>
  * @since 2019/09/05
  * @package Model\Dao
  */
-class SpotVIlla extends Dao
+class SpotVilla extends Dao
 { }
