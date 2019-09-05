3c8bc51b95779f8602209baf6d0f1e89becc0d04
wkmkymt <wkmkymt@gmail.com>
2019-09-05 16:48:51 +0900

    Add getter for user and topics

--- app/Model/Dao/Topic.php
+++ app/Model/Dao/Topic.php
@@ -13,4 +13,32 @@ namespace Model\Dao;
  * @package Model\Dao
  */
 class Topic extends Dao
-{ }
+{
+
+    /**
+     * getTopicList Function
+     *
+     * Topicテーブルにあるアイテム一覧を取得するためのサンプルです。
+     *
+     * @return array $result 結果情報を連想配列で指定します。
+     * @throws DBALException
+     * @author wkmkymt <wkmkymt@gmail.com>
+     * @since 2019/09/05
+     */
+    public function getTopicList()
+    {
+
+        // 全件取得するクエリを作成
+        $sql = "select villa.name, villa.description, villa.image_path
+                    from topic inner join villa on topic.villa_id = villa.id";
+
+        // SQLをプリペア
+        $statement = $this->db->prepare($sql);
+
+        // SQLを実行
+        $statement->execute();
+
+        // 結果レコードを全件取得し、返送
+        return $statement->fetchAll();
+    }
+}
