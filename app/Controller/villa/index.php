6712c36087cbb06055ce316b40dee7193ec58edd
wkmkymt <wkmkymt@gmail.com>
2019-09-05 10:59:34 +0900

    Init controller and view

---
+++ app/Controller/villa/index.php
@@ -0,0 +1,13 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+// Villaページのコントローラ
+$app->get('/', function (Request $request, Response $response) {
+
+  $data = [];
+
+  // Render index view
+  return $this->view->render($response, 'villa/index.twig', $data);
+});
