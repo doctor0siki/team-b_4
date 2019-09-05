e938e7ba9b5bd26fd542a100cafaaa9693b5c3fd
y-fukumoto <y-fukumoto@ceres-inc.jp>
2019-09-05 16:05:12 +0900

    add search logic

--- app/Model/Dao/Villa.php
+++ app/Model/Dao/Villa.php
@@ -2,6 +2,8 @@
 
 namespace Model\Dao;
 
+use PDO;
+
 /**
  * Class Villa
  *
@@ -13,4 +15,46 @@ namespace Model\Dao;
  * @package Model\Dao
  */
 class Villa extends Dao
-{ }
+{
+    /**
+     * searchByKeyword Function
+     *
+     * キーワード検索をおこないます。
+     * 対象は名称、詳細文言です。
+     *
+     * @param $keyword
+     * @return mixed
+     * @throws \Doctrine\DBAL\DBALException
+     * @copyright Ceres inc.
+     * @author y-fukumoto <y-fukumoto@ceres-inc.jp>
+     * @since 2019/09/05
+     */
+
+    public function searchByKeyword($keyword)
+    {
+
+        //SQL
+        $sql = "Select 
+                    id,
+                    name,
+                    image_path,
+                    LEFT(description,50) as description
+                 from villa 
+                where name like :keyword or description like :keyword";
+
+        // SQLをプリペア
+        $statement = $this->db->prepare($sql);
+
+        //部分一致検索にするため％で囲みます。
+        $keyword = "%" . $keyword . "%";
+
+        //キーワードを指定します
+        $statement->bindParam(":keyword", $keyword, PDO::PARAM_STR);
+
+        //SQLを実行
+        $statement->execute();
+
+        //結果レコードを一件取得し、返送
+        return $statement->fetchAll();
+    }
+}
