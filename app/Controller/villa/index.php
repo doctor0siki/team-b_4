5b81bd10a215d84e913d55103ed5825ae16627a9
wkmkymt <wkmkymt@gmail.com>
2019-09-05 13:35:42 +0900

    Delete getter

--- app/Controller/villa/index.php
+++ app/Controller/villa/index.php
@@ -9,14 +9,6 @@ $app->get('/villa/{ villa_id }[/]', function (Request $request, Response $respon
 
     $data = [];
 
-    // VillaのDAOをインスタンス化
-    $villa = new Villa($this->db);
-
-    // 一覧を取得し、戻り値をresultに格納します
-    $data = [
-        'villas' => $villa->getVillaList
-    ];
-
     // Render index view
     return $this->view->render($response, 'villa/index.twig', $data);
 });
