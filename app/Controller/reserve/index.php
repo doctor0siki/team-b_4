89295355332f82bb0631c2717250a2f23ee2dd88
Tenki <tenki.develop@gmail.com>
2019-09-06 11:44:21 +0900

    add_bootstrap

--- app/Controller/reserve/index.php
+++ app/Controller/reserve/index.php
@@ -4,10 +4,34 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 // Reserveページのコントローラ
-$app->get('/villa/reserve/{ villa_id }[/]', function (Request $request, Response $response, $args) {
+// 予約画面
+$app->get('/reserve/{ villa_id:[0-9+] }[/]', function (Request $request, Response $response, $args) {
 
-    $data = [];
-
+    $data = [
+        villa_id => $args["villa_id"]
+    ];
+    // $data = [];
+    // dd($data);
     // Render index view
     return $this->view->render($response, 'reserve/index.twig', $data);
+});
+
+// 予約確認画面
+$app->get('/reserve/confirm/{ villa_id }[/]', function (Request $request, Response $response, $args) {
+
+    //GETされた内容を取得します。
+    // $data = $request->getQueryParams();
+    $data = [];
+
+
+    // Render index view
+    return $this->view->render($response, 'reserve/check.twig', $data);
+});
+
+// 予約完了画面
+$app->get('/reserve/confirmed[/]', function (Request $request, Response $response) {
+
+    $data = [];
+    // Render index view
+    return $this->view->render($response, 'reserve/confirmed.twig', $data);
 });
