9e61da9d62d5620151436c8bce0ab7ed82eecf8c
y-fukumoto <y-fukumoto@ceres-inc.jp>
2019-09-06 14:00:55 +0900

    fix top page link and cut long string at 200 multi bytes Add title to menu.

--- app/Controller/top/index.php
+++ app/Controller/top/index.php
@@ -14,6 +14,11 @@ $app->get('/', function (Request $request, Response $response) {
         "topics" => $topic->getTopicList()
     ];
 
+    //デスクリプションの文字列を200字でカット。
+    foreach ($data["topics"] as $key => $val) {
+        $data["topics"][$key]["description"] = mb_strimwidth($val["description"], 0, 220, "...");
+    }
+
     // Render index view
     return $this->view->render($response, 'top/index.twig', $data);
 });
